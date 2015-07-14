f14cbed3d7db6fb9b38a71e93385bbf36c4ee96b
Lorenzo Bugiani <canemacchina@gmail.com>
2015-07-15 01:30:01 +0200

    suppaminor fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var stripDebug = require('gulp-strip-debug');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 
 gulp.task('uglify', function () {
   return gulp.src(['src/googleApi.module.js', 'src/providers/*.js'])
     .pipe(sourcemaps.init())
     .pipe(concat('angular-google-client.js'))
     .pipe(gulp.dest('example'))
-    .pipe(rename("angular-google-client.min.js"))
+    .pipe(rename('angular-google-client.min.js'))
     .pipe(stripDebug())
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
