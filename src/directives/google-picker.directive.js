3b6710f617368e9027d3385cf50758b509b4f92d
Lorenzo Bugiani <canemacchina@gmail.com>
2015-07-17 03:48:31 +0200

    Add google picker directive

--- src/directives/google-picker.directive.js
+++ src/directives/google-picker.directive.js
@@ -1,60 +1,72 @@
 (function() {
   'use strict';
-	angular.module('cm-google-api').directive('cmGooglePicker', ['googleClient', '$q', function(googleClient, $q){
-    var authDeferred = $q.defer();
-		return {
-			'restrict': 'A',
-			link: function (scope, element, attrs) {
-        var oauthToken = null;
-
-        function authUser() {
-          if(!oauthToken){
-            gapi.auth.authorize( { 'client_id': googleClient.clientId, 'scope': 'https://www.googleapis.com/auth/drive', 'immediate': false },  handleAuthResult);
-          }
-          return authDeferred.promise;
+  angular.module('cm-google-api').directive('cmGooglePicker', ['googleClient', '$q', '$window', function(googleClient, $q, $window){
+    var loading;
+    var authDeferred;
+    var oauthToken = null;
+    return {
+     restrict: 'A',
+     scope: {
+      scopes: '=',
+      locale: '@',
+      views: '&',
+      onPicked: '='
+    },
+    link: function (scope, element, attrs) {
+      function authUser() {
+        if(!loading && !oauthToken){
+          authDeferred = $q.defer();
+          gapi.auth.authorize( { 'client_id': googleClient.clientId, 'scope': scope.scopes, 'immediate': false },  handleAuthResult);
         }
+        return authDeferred.promise;
+      }
 
-        function handleAuthResult(authResult) {
-          if (authResult && !authResult.error) {
-            oauthToken = authResult.access_token;
-            authDeferred.resolve();
-          }
+      function handleAuthResult(authResult) {
+        loading = false;
+        if (authResult && !authResult.error) {
+          oauthToken = authResult.access_token;
+          authDeferred.resolve();
+        }else{
+          authDeferred.reject();
         }
+      }
 
-        function pickerCallback (data) {
-          console.log(data.action);
-          if (data.action == google.picker.Action.LOADED) {
-            console.log(data);
-          }
-          if (data.action == google.picker.Action.CANCEL) {
-            console.log(data);
-          }
-          if (data.action == google.picker.Action.PICKED) {
-            console.log(data);
-          }
+      function pickerCallback (data) {
+        if (data.action === google.picker.Action.PICKED) {
+          scope.onPicked(data.docs);
         }
+      }
 
-        function openPicker(){
-          googleClient.afterScriptsLoaded().then(
-            function(){
-              authUser().then(function(){
-                var picker = new google.picker.PickerBuilder().
-                  addView(google.picker.ViewId.DOCS).
-                  setOAuthToken(oauthToken).
-                  setCallback(pickerCallback).
-                  build();
-                picker.setVisible(true);
+      function openPicker(){
+        googleClient.afterScriptsLoaded().then(
+          function(){
+            authUser().then(function(){
+              var picker = new google.picker.PickerBuilder()
+              .setLocale(scope.locale)
+              .setOAuthToken(oauthToken)
+              .setOrigin($window.location.protocol + '//' + $window.location.host)
+              .setCallback(pickerCallback);
+              var viewArray = scope.views();
+              angular.forEach(viewArray, function(view){
+                picker.addView(view);
               });
-            },
-            function(){}
-          );
-        }
+              picker = picker.build();
+              picker.setVisible(true);
+            });
+          }
+        );
+      }
 
-        element.bind('click', function (e) {
-          console.log('click')
-          openPicker();
-        });
-    	}
-		}
-	}]);
+      loading = true;
+      googleClient.afterScriptsLoaded().then(function(){
+        authDeferred = $q.defer();
+        gapi.auth.authorize( { 'client_id': googleClient.clientId, 'scope': scope.scopes, 'immediate': true },  handleAuthResult);
+      });
+
+      element.bind('click', function (e) {
+        openPicker();
+      });
+    }
+  };
+}]);
 })();
